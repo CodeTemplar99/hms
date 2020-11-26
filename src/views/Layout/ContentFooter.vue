35cc860f8ad35cef5f0978008cb48b7ad33040de
Codetemplar99 <udoagwac@gmail.com>
2020-11-26 13:50:22 +0100

    all on

---
+++ src/views/Layout/ContentFooter.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer class="footer px-4">
+    <b-row align-v="center" class="justify-content-lg-end">
+      <b-col lg="12">
+        <div class="copyright text-center text-lg-right text-muted">
+          © {{year}} <span class="font-weight-bold ml-1">UNN</span>
+        </div>
+      </b-col>
+    </b-row>
+  </footer>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      year: new Date().getFullYear()
+    };
+  }
+};
+</script>
+<style></style>
