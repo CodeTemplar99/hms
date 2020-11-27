dc83c5b49443fcd3f412bff4227a26dcdc299c21
Codetemplar99 <udoagwac@gmail.com>
2020-11-27 08:19:08 +0100

    profile detail

--- src/views/Pages/UserProfile/UserCard.vue
+++ src/views/Pages/UserProfile/UserCard.vue
@@ -20,37 +20,37 @@
     <b-card-body class="pt-0">
       <b-row>
         <b-col >
-          <div class="card-profile-stats d-flex justify-content-center mt-md-5">
+          <div class="card-profile-stats d-flex justify-content-center mt-md-6">
             <div>
-              <span class="heading">22</span>
-              <span class="description">Friends</span>
+              <span class="heading">200</span>
+              <span class="description">Level</span>
             </div>
             <div>
-              <span class="heading">10</span>
-              <span class="description">Photos</span>
+              <span class="heading">2019/2020</span>
+              <span class="description">Session</span>
             </div>
-            <div>
-              <span class="heading">89</span>
-              <span class="description">Comments</span>
+              <div>
+              <span class="heading">2ND</span>
+              <span class="description">Semester</span>
             </div>
           </div>
         </b-col>
       </b-row>
       <div class="text-center">
         <h5 class="h3">
-          Jessica Jones<span class="font-weight-light">, 27</span>
+          Kelechi Pyragle
         </h5>
-        <div class="h5 font-weight-300">
-          <i class="ni location_pin mr-2"></i>Bucharest, Romania
+        <div class="h4 font-weight-300">
+          <i class="ni location_pin mr-2"></i>Computer Science
         </div>
-        <div class="h5 mt-4">
-          <i class="ni business_briefcase-24 mr-2"></i>Solution Manager - Creative Tim Officer
+        <div class="h3 mt-4">
+          <i class="ni business_briefcase-24 mr-2"></i>Physical Science
         </div>
         <div>
           <i class="ni education_hat mr-2"></i>University of Computer Science
         </div>
         <hr class="my-4">
-        <p>Ryan — the name taken by Melbourne-raised, Brooklyn-based Nick Murphy — writes, performs and records all of his own music.</p>
+        <p>--profile details gotten from student portal ---Please refer to student portal for changes</p>
         <!-- <a href="#">Show more</a> -->
 
       </div>
