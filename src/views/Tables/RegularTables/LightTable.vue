400d29b3e44e49e94c498226d048e655f9471bc1
Codetemplar99 <udoagwac@gmail.com>
2020-11-27 12:09:09 +0100

    activity view

--- src/views/Tables/RegularTables/LightTable.vue
+++ src/views/Tables/RegularTables/LightTable.vue
@@ -1,27 +1,24 @@
 <template>
     <b-card no-body>
         <b-card-header class="border-0">
-            <h3 class="mb-0">Light table</h3>
+            <h3 class="mb-0">Activity Table</h3>
         </b-card-header>
 
         <el-table class="table-responsive table"
                   header-row-class-name="thead-light"
                   :data="projects">
-            <el-table-column label="Project"
+            <el-table-column label="Hostel"
                              min-width="310px"
                              prop="name">
                 <template v-slot="{row}">
                     <b-media no-body class="align-items-center">
-                        <a href="#" class="avatar rounded-circle mr-3">
-                            <img alt="Image placeholder" :src="row.img">
-                        </a>
                         <b-media-body>
                             <span class="font-weight-600 name mb-0 text-sm">{{row.title}}</span>
                         </b-media-body>
                     </b-media>
                 </template>
             </el-table-column>
-            <el-table-column label="Budget"
+            <el-table-column label="Room"
                              prop="budget"
                              min-width="140px">
             </el-table-column>
@@ -37,28 +34,7 @@
                 </template>
             </el-table-column>
 
-            <el-table-column label="Users" min-width="190px">
-                <div class="avatar-group">
-                    <a href="#" class="avatar avatar-sm rounded-circle" data-toggle="tooltip"
-                       data-original-title="Ryan Tompson">
-                        <img alt="Image placeholder" src="img/theme/team-1.jpg">
-                    </a>
-                    <a href="#" class="avatar avatar-sm rounded-circle" data-toggle="tooltip"
-                       data-original-title="Romina Hadid">
-                        <img alt="Image placeholder" src="img/theme/team-2.jpg">
-                    </a>
-                    <a href="#" class="avatar avatar-sm rounded-circle" data-toggle="tooltip"
-                       data-original-title="Alexander Smith">
-                        <img alt="Image placeholder" src="img/theme/team-3.jpg">
-                    </a>
-                    <a href="#" class="avatar avatar-sm rounded-circle" data-toggle="tooltip"
-                       data-original-title="Jessica Doe">
-                        <img alt="Image placeholder" src="img/theme/team-4.jpg">
-                    </a>
-                </div>
-            </el-table-column>
-
-            <el-table-column label="Completion"
+            <el-table-column label="Time left"
                              prop="completion"
                              min-width="240px">
                 <template v-slot="{row}">
@@ -72,9 +48,9 @@
             </el-table-column>
         </el-table>
 
-        <b-card-footer class="py-4 d-flex justify-content-end">
+        <!-- <b-card-footer class="py-4 d-flex justify-content-end">
             <base-pagination v-model="currentPage" :per-page="10" :total="50"></base-pagination>
-        </b-card-footer>
+        </b-card-footer> -->
     </b-card>
 </template>
 <script>
--- src/views/Tables/projects.js
+++ src/views/Tables/projects.js
@@ -1,75 +1,24 @@
 
 export default [
   {
-    img: 'img/theme/bootstrap.jpg',
-    title: 'Argon Design System',
-    budget: '$2500 USD',
-    status: 'pending',
-    statusType: 'warning',
-    completion: 60
+    title: 'Okeke',
+    budget: 'Room 304',
+    status: 'Allocated',
+    statusType: 'success',
+    completion: 0
   },
   {
-    img: 'img/theme/angular.jpg',
-    title: 'Angular Now UI Kit PRO',
-    budget: '$1800 USD',
-    status: 'completed',
-    statusType: 'success',
+    title: 'Eni Njoku',
+    budget: 'Room 307',
+    status: 'Forfeited',
+    statusType: 'danger',
     completion: 100
   },
   {
-    img: 'img/theme/sketch.jpg',
-    title: 'Black Dashboard',
-    budget: '$3150 USD',
-    status: 'delayed',
-    statusType: 'danger',
-    completion: 72
-  },
-  {
-    img: 'img/theme/react.jpg',
-    title: 'React Material Dashboard',
-    budget: '$4400 USD',
-    status: 'on schedule',
-    statusType: 'info',
+    title: 'Bello',
+    budget: 'Room 404',
+    status: 'Reserved',
+    statusType: 'warning',
     completion: 90
-  },
-  {
-    img: 'img/theme/vue.jpg',
-    title: 'Vue Paper UI Kit PRO',
-    budget: '$2200 USD',
-    status: 'completed',
-    statusType: 'success',
-    completion: 100
-  },
-  {
-    img: 'img/theme/bootstrap.jpg',
-    title: 'Argon Design System',
-    budget: '$2500 USD',
-    status: 'pending',
-    statusType: 'warning',
-    completion: 60
-  },
-  {
-    img: 'img/theme/angular.jpg',
-    title: 'Angular Now UI Kit PRO',
-    budget: '$1800 USD',
-    status: 'completed',
-    statusType: 'success',
-    completion: 100
-  },
-  {
-    img: 'img/theme/sketch.jpg',
-    title: 'Black Dashboard',
-    budget: '$3150 USD',
-    status: 'delayed',
-    statusType: 'danger',
-    completion: 72
-  },
-  {
-    img: 'img/theme/vue.jpg',
-    title: 'Vue Paper UI Kit PRO',
-    budget: '$2200 USD',
-    status: 'completed',
-    statusType: 'success',
-    completion: 100
   }
 ]
