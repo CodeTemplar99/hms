b61c1b3f35bf70e57cd3a4d464e6e0f49ff7bac6
Codetemplar99 <udoagwac@gmail.com>
2020-11-29 11:31:26 +0100

    remove forgot pass under

--- src/views/Pages/Login.vue
+++ src/views/Pages/Login.vue
@@ -56,10 +56,7 @@
             </b-card-body>
           </b-card>
           <b-row class="mt-3">
-            <b-col cols="6">
-              <router-link to="/dashboard" class="text-light"><small>Forgot password?</small></router-link>
-            </b-col>
-            <b-col cols="6" class="text-right">
+            <b-col cols="12" class="text-right">
               <router-link to="/register" class="text-light"><small>Create new account</small></router-link>
             </b-col>
           </b-row>
