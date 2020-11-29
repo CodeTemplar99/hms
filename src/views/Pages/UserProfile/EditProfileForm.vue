cb666b112e1a80ef924849f7f6a1a77913cbba99
Codetemplar99 <udoagwac@gmail.com>
2020-11-29 16:46:51 +0100

    password change in profile

--- src/views/Pages/UserProfile/EditProfileForm.vue
+++ src/views/Pages/UserProfile/EditProfileForm.vue
@@ -1,10 +1,10 @@
 <template>
   <card>
-    <b-row align-v="center" slot="header" >
+    <b-row align-v="center" slot="header">
       <b-col cols="8">
-        <h3 class="mb-0">Edit profile </h3>
+        <h3 class="mb-0">Edit profile</h3>
       </b-col>
-     </b-row>
+    </b-row>
 
     <b-form @submit.prevent="updateProfile">
       <h6 class="heading-small text-muted mb-4">User information</h6>
@@ -30,7 +30,7 @@
             </base-input>
           </b-col>
         </b-row>
-        <b-row >
+        <b-row>
           <b-col lg="6">
             <base-input
               type="text"
@@ -51,7 +51,7 @@
           </b-col>
         </b-row>
       </div>
-      <hr class="my-4">
+      <hr class="my-4" />
 
       <!-- Address -->
       <h6 class="heading-small text-muted mb-4">Contact information</h6>
@@ -98,6 +98,36 @@
         </b-row>
       </div>
     </b-form>
+
+    <h6 class="heading-small text-muted mb-4">Security</h6>
+
+    <div class="pl-lg-4">
+     <b-row>
+        <b-col lg="4">
+          <base-input
+            type="text"
+            label="Old Password"
+            placeholder="Old Password"
+          >
+          </base-input>
+        </b-col>
+        <b-col lg="4">
+          <base-input
+            type="text"
+            label="New Password"
+            placeholder="New Password"
+          >
+          </base-input>
+        </b-col>
+        <b-col lg="4">
+          <base-input
+            label="Confirm Password"
+            placeholder="Confirm Password"
+          >
+          </base-input>
+        </b-col>
+      </b-row>
+    </div>
   </card>
 </template>
 <script>
@@ -105,24 +135,24 @@ export default {
   data() {
     return {
       user: {
-        company: 'Creative Code Inc.',
-        username: 'michael23',
-        email: '',
-        firstName: 'Mike',
-        lastName: 'Andrew',
-        address: 'Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09',
-        city: 'New York',
-        country: 'USA',
-        postalCode: '',
-        aboutMe: `Lamborghini Mercy, Your chick she so thirsty, I'm in that two seat Lambo.`
-      }
+        company: "Creative Code Inc.",
+        username: "michael23",
+        email: "",
+        firstName: "Mike",
+        lastName: "Andrew",
+        address: "Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09",
+        city: "New York",
+        country: "USA",
+        postalCode: "",
+        aboutMe: `Lamborghini Mercy, Your chick she so thirsty, I'm in that two seat Lambo.`,
+      },
     };
   },
   methods: {
     updateProfile() {
-      alert('Your data: ' + JSON.stringify(this.user));
-    }
-  }
+      alert("Your data: " + JSON.stringify(this.user));
+    },
+  },
 };
 </script>
 <style></style>
