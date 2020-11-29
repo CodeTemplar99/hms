2318bda05d59d905c1ba4deb51b14ebca3118949
Codetemplar99 <udoagwac@gmail.com>
2020-11-29 11:51:19 +0100

    finish with recovery

--- src/views/Pages/AuthLayout.vue
+++ src/views/Pages/AuthLayout.vue
@@ -53,28 +53,12 @@
     </div>
 
     <footer class="py-5" id="footer-main">
-      <b-container >
-        <b-row align-v="center" class="justify-content-xl-between">
-          <b-col xl="6">
+      <b-container class="d-flex justify-content-end align-items-baseline" >
+        <b-row align-v="center" class="justify-content-xl-start">
+          <b-col xl="12">
             <div class="copyright text-center text-xl-left text-muted">
-              © {{year}} <a href="https://www.creative-tim.com" class="font-weight-bold ml-1" target="_blank">Creative Tim</a>
+              © {{year}} <a href="#" class="font-weight-bold ml-1" target="_blank">UNN</a>
             </div>
-          </b-col>
-          <b-col xl="6" class="col-xl-6">
-            <b-nav  class="nav-footer justify-content-center justify-content-xl-end">
-              <b-nav-item href="https://www.creative-tim.com" target="_blank" >
-                Creative Tim
-              </b-nav-item>
-              <b-nav-item href="https://www.creative-tim.com/presentation" target="_blank" >
-                About Us
-              </b-nav-item>
-              <b-nav-item href="http://blog.creative-tim.com"  target="_blank">
-                Blog
-              </b-nav-item>
-              <b-nav-item href="https://www.creative-tim.com/license" target="_blank">
-                License
-              </b-nav-item>
-            </b-nav>
           </b-col>
         </b-row>
       </b-container>
